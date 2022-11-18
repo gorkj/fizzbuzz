6417e4bd10c519cdd112c9f15b7a7a1d02662cfb
Göran Kjellén <goran.kjellen@wirelesscar.com>
2022-11-18 12:48:39 +0100

    Maven project

--- java/src/main/java/gorkj/Fizzbuzz.java
+++ java/src/main/java/gorkj/Fizzbuzz.java
@@ -1,6 +1,8 @@
+package gorkj;
+
 class Fizzbuzz {
     public static void main(String[] args) {
-        for (int i=0;i<=100;i++) {
+        for (int i=1;i<=100;i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
